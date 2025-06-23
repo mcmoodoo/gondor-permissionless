1f89a67c3e4050b3ecb9f4032e7a15249a721a83
mcmoodoo <rashid.makhmud@gmail.com>
2025-06-22 21:44:11 -0400

    changed erc20 to 1155 on polygon lock side

--- src/PolygonBridge.sol
+++ src/PolygonBridge.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PolygonBridge is ReentrancyGuard, Ownable {
-    IERC20 public token;
+contract PolygonBridge is ReentrancyGuard, Ownable, ERC1155Holder {
+    IERC1155 public token;
     
     event TokensLocked(
         address indexed user,
+        uint256 indexed tokenId,
         uint256 amount,
         string destinationAddress,
         uint256 indexed nonce
@@ -19,17 +21,17 @@ contract PolygonBridge is ReentrancyGuard, Ownable {
     uint256 public currentNonce;
     
     constructor(address _token) Ownable(msg.sender) {
-        token = IERC20(_token);
+        token = IERC1155(_token);
     }
     
-    function lockTokens(uint256 amount, string memory destinationAddress) 
+    function lockTokens(uint256 tokenId, uint256 amount, string memory destinationAddress) 
         external 
         nonReentrant 
     {
         require(amount > 0, "Amount must be positive");
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        token.safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
         
         currentNonce++;
-        emit TokensLocked(msg.sender, amount, destinationAddress, currentNonce);
+        emit TokensLocked(msg.sender, tokenId, amount, destinationAddress, currentNonce);
     }
 }
